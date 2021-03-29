63cc7bb5159352d1876c6c51e6fb6f66e8d40043
ritam14 <ritamsingha1992@gmail.com>
2021-03-29 21:38:55 +0530

    sdfsdf

--- src/javaPractice/Practice.java
+++ src/javaPractice/Practice.java
@@ -5,7 +5,6 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Brothers!!");
-		System.out.println("Vote for CPIM!!");
 		System.out.println("Hellooo Brothers!!");
 
 	}
