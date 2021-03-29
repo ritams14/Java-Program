7468c5f7ec6c08eb962a21dfb2ab5f855011a4e1
ritam14 <ritamsingha1992@gmail.com>
2021-03-29 21:36:55 +0530

    git commit

--- src/javaPractice/Practice.java
+++ src/javaPractice/Practice.java
@@ -5,6 +5,8 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Brothers!!");
+		System.out.println("Vote for CPIM!!");
+		System.out.println("Hellooo Brothers!!");
 
 	}
 
